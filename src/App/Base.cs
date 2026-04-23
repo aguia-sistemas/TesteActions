df310e8401bdcd639d4aa8620b06c9fa1880dff5
leandro <leandrosalamaia@gmail.com>
2026-04-23 10:09:31 -0300

    Insert Method Value3

--- src/App/Base.cs
+++ src/App/Base.cs
@@ -5,4 +5,6 @@ public class Teste
     public string Valor(string valor) => $"valor, {valor}!";
     
     public string Valor2(string valor) => $"valor2, {valor}!";
+    
+    public string Valor3(string valor) => $"valor3, {valor}!";
 }
